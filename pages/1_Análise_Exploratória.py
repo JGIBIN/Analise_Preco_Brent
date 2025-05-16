91842db08ae83e1604ea1a595d17938e38fab800
Jorge Gibin <jorgegibin@outlook.com.br>
2025-05-16 09:29:35 -0300

    t

--- pages/1_Análise_Exploratória.py
+++ pages/1_Análise_Exploratória.py
@@ -62,9 +62,9 @@ fig_hist_ma.add_trace(go.Scatter(x=df_historical_10a['Data'], y=df_historical_10
 eventos = [
     {'Data': '2014-11-27', 'descricao': 'OPEP decide manter produção, iniciando queda de preços', 'color': 'white', 'ay_offset': -40},
     {'Data': '2016-01-20', 'descricao': 'Preço atinge mínima da década após decisão da OPEP', 'color': 'white', 'ay_offset': -70},
-    {'Data': '2020-03-11', 'descricao': 'COVID-19 declarada pandemia global', 'color': 'white', 'ay_offset': -100},
-    {'Data': '2020-04-20', 'descricao': 'Preço do petróleo WTI fica negativo (afeta Brent)', 'color': 'white', 'ay_offset': -130},
-    {'Data': '2022-02-24', 'descricao': 'Início da Guerra na Ucrânia', 'color': 'white', 'ay_offset': -160}
+    {'Data': '2020-03-11', 'descricao': 'COVID-19 declarada pandemia global', 'color': 'white', 'ay_offset': -40},
+    {'Data': '2020-04-20', 'descricao': 'Preço do petróleo WTI fica negativo (afeta Brent)', 'color': 'white', 'ay_offset': -70},
+    {'Data': '2022-02-24', 'descricao': 'Início da Guerra na Ucrânia', 'color': 'white', 'ay_offset': -40}
 ]
 eventos_filtrados_plot_ma = [e for e in eventos if pd.to_datetime(e['Data']) >= df_historical_10a['Data'].min() and pd.to_datetime(e['Data']) <= df_historical_10a['Data'].max()]
 annotations_list_ma = []
