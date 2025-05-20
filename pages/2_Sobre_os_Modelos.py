738e9b248539f5852bbf248cdc4dd65ef3f41568
Jorge Gibin <jorgegibin@outlook.com.br>
2025-05-20 17:58:54 -0300

    Update 2_Sobre_os_Modelos.py

--- pages/2_Sobre_os_Modelos.py
+++ pages/2_Sobre_os_Modelos.py
@@ -35,6 +35,7 @@ LSTM é um tipo de rede neural muito bom para lidar com dados ao longo do tempo,
     * Consegue "lembrar" de coisas que aconteceram há muito tempo nos dados,
     * Lida bem com relações complicadas que os modelos antigos não enxergam,
     * Pode usar várias informações ao mesmo tempo (como dia da semana, mês, etc.).
+    
 
 * **Por que usamos o LSTM junto com o ARIMAX:**
     1. O modelo ARIMAX nos dá uma boa base, capturando padrões mais simples.
